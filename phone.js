68922894e0ae670595989373a67d69cbe32aecd4
keya12345 <0319452930@student.wub.edu.bd>
2022-03-02 08:55:50 +0600

    new commit

--- phone.js
+++ phone.js
@@ -1,9 +1,13 @@
-//load phone:
+//spinner:
+const toggleSpinner=displayStyle=>{
+    document.getElementById('spinner').style.display=displayStyle
+}
 
-        
-        secondError=''
+//load phone:
 const loadPhone=()=>{
     const searchBox = document.getElementById('search-box')
+    //display spinner
+    toggleSpinner('block')
     const searchText= searchBox.value
     searchBox.value= ''
     if(searchText.length===0){
@@ -15,7 +19,7 @@ const loadPhone=()=>{
         
     } 
 else{
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText.toLowerCase()}`
    fetch(url)
    .then(res=>res.json())
    .then(data=>displayLoad(data.data))
@@ -23,6 +27,8 @@ else{
 
 }
 
+
+// display phone card
 const displayLoad=(phones)=>{
     const main = document.getElementById('main')
     main.textContent=''
@@ -34,12 +40,12 @@ const displayLoad=(phones)=>{
         firstError=''
         
     }
+   
 
     
-
- phones.forEach(phone => {
-     //console.log(phone)
-     //console.log(phone.slug)
+//forEach use
+ phones.slice(0,20).forEach(phone => {
+     
     const div=document.createElement('div')
     div.classList.add('col')
     div.innerHTML=`<div  class="card w-75" >
@@ -48,7 +54,7 @@ const displayLoad=(phones)=>{
     <h4 class="card-title text-primary">${phone.brand}</h4>
     <h3 class="card-title text-success">${phone.phone_name}</h3>
     <button onclick="phoneDetails('${phone.slug}')" class="border-info rounded bg-primary  fw-bold text-white">phone-details</button>
-    if(phone.slice())
+    
      </div>
      </div>`
      main.appendChild(div)
@@ -58,6 +64,7 @@ const displayLoad=(phones)=>{
 
 }
 
+//phone details id load
 const phoneDetails=(id)=>{
     
     const url=`https://openapi.programming-hero.com/api/phone/${id}`
@@ -67,19 +74,35 @@ const phoneDetails=(id)=>{
     .then(data=>displayDetails(data.data))
 }
 
+//display details phone
 const displayDetails=(details)=>{
-    //console.log(details)
+    console.log(details)
    const detailsSite =document.getElementById('details-site')
    detailsSite.textContent=''
    const div=document.createElement('div')
    div.classList.add('card')
    div.innerHTML=`<img class="w-25 text-center mx-auto" src="${details.image}" class="card-img-top" alt="...">
    <div class="card-body">
-     <h5 class="card-title">${details.mainFeatures.chipSet}</h5>
-     <h6 class="card-text">${details.mainFeatures.memory}</h6>
-     <h6 class="card-text">${details.mainFeatures.sensors.slice(0,5)}</h6>
+   <h5>Name: ${details.name}</h5>
+   <h5>Brand: ${details.brand}</h5>
+   <h4 class="text-info fw-bold" >MainFeatures:</h4>
+   <h6>Release Date: ${details.releaseDate?details.releaseDate:'no releasedate found'}</h6>
+     <h6 class="card-title">Chipset: ${details.mainFeatures.chipSet}</h6>
+     <h6 class="card-title">Displaysize: ${details.mainFeatures.displaySize}</h6>
+     <h6 class="card-text">Memory: ${details.mainFeatures.memory}</h6>
+     <h6 class="card-text">Sensors: ${details.mainFeatures.sensors.slice(0,5)}</h6>
+     <h6>Slug: ${details.slug}</h6>
      
-     <p>${details.releaseDate?details.releaseDate:'no releasedate found'}</p>
+        
+       
+        <h4 class="text-info fw-bold">Others:</h4>
+        <h5>Bluetooth: ${details.others.Bluetooth}</h5>
+        <h5>GPS: ${details.others.GPS}</h5>
+        <h5>NFC: ${details.others.NFC}</h5>
+        <h5>Radio: ${details.others.Radio}</h5>
+        <h5>USB: ${details.others.USB}</h5>
+        <h5>WLAN: ${details.others.WLAN}</h5>
+      
      
    </div>
    `
